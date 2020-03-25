02ba3ecab71120691fd3d285613670c869de0769
bolheinmin <bolheinmin@gmail.com>
2020-03-25 18:25:35 +0630

    test

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const APP_URL = "https://fbstarterbot.herokuapp.com";
+const APP_URL = "https://myfoodiebot.herokuapp.com";
 
 // Imports dependencies and set up http server
 const 
